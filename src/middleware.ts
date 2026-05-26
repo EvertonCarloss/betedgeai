fd124f1eef2de6e67e3894868eb8f994e15b745c
Everton Carlos <Evertonlensson@hotmail.com>
2026-05-26 20:25:38 -0300

    fix: disable middleware temporarily

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,9 @@
-import { createServerClient } from '@supabase/ssr';
-import { NextResponse, type NextRequest } from 'next/server';
+import { type NextRequest } from 'next/server';
 
-export async function updateSession(request: NextRequest) {
-  const supabaseResponse = NextResponse.next({ request });
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options),
-          );
-        },
-      },
-    },
-  );
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  const isAuthPage = request.nextUrl.pathname.startsWith('/auth');
-
-  if (!user && !isAuthPage) {
-    const url = request.nextUrl.clone();
-    url.pathname = '/auth/login';
-    return NextResponse.redirect(url);
-  }
-
-  return supabaseResponse;
+export async function middleware(request: NextRequest) {
+  return;
 }
+
+export const config = {
+  matcher: [],
+};
